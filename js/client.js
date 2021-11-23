51b8d0eec2e418f22c45bf63ec9c1e5d630b5c4a
Kallyan Singha <singhakallyan@gmail.com>
2021-11-23 19:21:41 +0530

    addded changes

--- js/client.js
+++ js/client.js
@@ -3,15 +3,15 @@ const socket = io('http://localhost:8000');
 const sbtn = document.getElementById('sendbtn');
 const messageInput = document.getElementById('chatbox')
 const messageContainer = document.getElementById('txtmsg')
-let inpbtn= document.getElementById('inpbtn');
-let inpname= document.getElementById('inpname');
+let inpbtn = document.getElementById('inpbtn');
+let inpname = document.getElementById('inpname');
 
-const append = (name,message,position)=>{
+const append = (name, message, position) => {
     const messageElement = document.createElement('div');
     const messageName = document.createElement('div');
     // const messageTime = document.createElement('div');
-    messageName.innerText=name;
-    messageElement.innerText= message;
+    messageName.innerText = name;
+    messageElement.innerText = message;
     messageName.classList.add('msgname');
     messageElement.appendChild(messageName);
     messageElement.classList.add('messages');
@@ -19,39 +19,39 @@ const append = (name,message,position)=>{
     messageContainer.append(messageElement);
 }
 
-const wlcmmsg=(name)=>{
+const wlcmmsg = (name) => {
     const messageElement = document.createElement('div');
     const appearMsg = document.createElement('div');
-    appearMsg.innerHTML=`${name} Joined The Chat`
+    appearMsg.innerHTML = `${name} Joined The Chat`
     appearMsg.classList.add('appearmsg');
     messageElement.append(appearMsg);
     messageContainer.append(messageElement);
 }
-sbtn.addEventListener('click',(e)=>{
-    const message= messageInput.value;
-    append(`You` ,`${message}`,'right');
-    socket.emit('send',message);
+sbtn.addEventListener('click', (e) => {
+    const message = messageInput.value;
+    append(`You`, `${message}`, 'right');
+    socket.emit('send', message);
     messageInput.value = '';
 })
 
 // const name = prompt("Enter Your Name:");
-function givename(){
-inpbtn.addEventListener('click',nameinput)
-async function nameinput(){
-    
-    let name = inpname.value;
-    console.log(name)
+async function givename() {
+    inpbtn.addEventListener('click', nameinput)
+    await function nameinput() {
+
+        let name = inpname.value;
+        console.log(name)
+        Promise.resolve()
+    }
 }
-}
-const a= nameinput();
-a.then((name)=>{
-    socket.emit('new-user-joined' , name);
-     socket.on('user-joined',name=>{
+
+socket.emit('new-user-joined', name);
+socket.on('user-joined', name => {
     // append(" ",`${name} Joined the chat`,"right")
     wlcmmsg(name);
-    })
-     socket.on('receive',data=>{
-    append(`${data.name}:`, `${data.message} `,"left")
-    })
 })
+socket.on('receive', data => {
+    append(`${data.name}:`, `${data.message} `, "left")
+})
+}
 
